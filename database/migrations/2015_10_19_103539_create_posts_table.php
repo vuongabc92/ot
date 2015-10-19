4215c01606e7fab0e727dd8cf514f5585e22039d
vuong bui <vuong.bui@sutrixmedia.com>
2015-10-19 18:03:09 +0700

    Init posts

---
+++ database/migrations/2015_10_19_103539_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->string('name', 250);
+            $table->string('image', 250);
+            $table->string('image_thumbnail', 250);
+            $table->text('content');
+            $table->text('short_content');
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
